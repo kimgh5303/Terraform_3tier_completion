47e6bd56c2cb407842356cf603787c3e89fd4a0f
kimgh5303 <kimgh5303@naver.com>
2024-05-25 08:40:25 +0000

    0525

--- rds.tf
+++ rds.tf
@@ -31,7 +31,7 @@ resource "aws_db_instance" "rds-db" {
   username               = var.db-username
   password               = var.db-password
   parameter_group_name   = aws_db_parameter_group.mysql8-parameter-group.name
-  multi_az               = false   # multi-az 추후 활성화 할 것
+  multi_az               = true                                                                # multi-az
   db_subnet_group_name   = aws_db_subnet_group.subnet-grp.name
   vpc_security_group_ids = [aws_security_group.db-sg.id]
   skip_final_snapshot    = true
