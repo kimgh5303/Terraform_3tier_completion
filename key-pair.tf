3744448585200821c676aadd91889380f8ded0f1
kimgh5303 <kimgh5303@naver.com>
2024-05-27 08:04:35 +0000

    중간평가까지 구성 완료

--- key-pair.tf
+++ key-pair.tf
@@ -5,3 +5,25 @@
 #  key_name   = var.key-name
 #  public_key = file("3tier-key.pub")
 #}
+
+# SSH 키 생성
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# SSH 키를 AWS에 등록
+resource "aws_key_pair" "deployer" {
+  key_name   = "ssh-key"
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+# SSH 키를 로컬 파일로 저장
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.private_key.private_key_pem
+  filename = "${path.module}/ec2_key.pem"
+}
+
+output "private_key_path" {
+  value = local_file.private_key_pem.filename
+}
