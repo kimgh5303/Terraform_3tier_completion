75b6072ac9df481b4a1aef66ed87846741147c31
kimgh5303 <kimgh5303@naver.com>
2024-05-28 10:11:02 +0000

    세부사항 수정

--- key-pair.tf
+++
@@ -1,7 +0,0 @@
-# 키 예시
-# ssh-keygen -t rsa -b 4096 -C "" -f "/root/terraform/multi-tier-architecture-using-terraform/3tier-key" -N ""
-
-#resource "aws_key_pair" "my-key" {
-#  key_name   = var.key-name
-#  public_key = file("3tier-key.pub")
-#}
